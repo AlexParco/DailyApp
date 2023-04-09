8ab727e1d8e035c16206f1cfb1bb9005f1365f2b
AlexParco <alexparco16@gmail.com>
2023-04-08 20:56:15 -0500

    feat: login screen

--- App.js
+++ App.js
@@ -7,6 +7,8 @@ import Main from "./src/screens/Main.js";
 import AddTask from "./src/screens/Add.js";
 import Settings from "./src/screens/Setttings.js";
 import { Feather } from '@expo/vector-icons';
+import Login from "./src/screens/Access/Login.js";
+import { AuthContextProvider } from "./src/context/AuthContext.js";
 
 
 const Stack = createNativeStackNavigator();
@@ -14,35 +16,41 @@ const Stack = createNativeStackNavigator();
 export default function App() {
   return (
     <>
-      <NavigationContainer >
-        <Stack.Navigator
-          initialRouteName="Main"
-          screenOptions={{
-            headerShown: false,
-          }}>
-          <Stack.Screen
-            name="Main"
-            component={Main}
-            options={{
-              tabBarIcon: ({ color, size }) => (
-                <Feather name="server" size={size} color={color} />
-              )
-            }}
-          />
+      <AuthContextProvider>
 
-          <Stack.Screen
-            name="Add"
-            component={AddTask}
-            options={{
-              presentation: 'card'
-            }}
-          />
-          <Stack.Screen
-            name="Settings"
-            component={Settings}
-          />
-        </Stack.Navigator>
-      </NavigationContainer >
+        <NavigationContainer >
+          <Stack.Navigator
+            initialRouteName="Login"
+            screenOptions={{
+              headerShown: false,
+            }}>
+            <Stack.Screen
+              name="Login"
+              component={Login}
+            />
+            <Stack.Screen
+              name="Main"
+              component={Main}
+              options={{
+                tabBarIcon: ({ color, size }) => (
+                  <Feather name="server" size={size} color={color} />
+                )
+              }}
+            />
+            <Stack.Screen
+              name="Add"
+              component={AddTask}
+              options={{
+                presentation: 'card'
+              }}
+            />
+            <Stack.Screen
+              name="Settings"
+              component={Settings}
+            />
+          </Stack.Navigator>
+        </NavigationContainer >
+      </AuthContextProvider>
     </>
   )
 }
